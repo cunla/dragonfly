d0fc988713bfcfd25d81e06624e756cb688eb8ee
Daniel M <daniel.maruani@gmail.com>
2024-10-29 11:20:40 -0400

    update to py3.11

--- tests/fakeredis/test/test_asyncredis.py
+++ tests/fakeredis/test/test_asyncredis.py
@@ -2,7 +2,10 @@ import asyncio
 import sys
 
 
-from asyncio import timeout as async_timeout
+if sys.version_info >= (3, 11):
+    from asyncio import timeout as async_timeout
+else:
+    from async_timeout import timeout as async_timeout
 import pytest
 import pytest_asyncio
 import redis
